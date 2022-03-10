68a555705e35b04056656dfc3acdac5dee2badea
soumya <soumyaranjanparhilipu5@gmail.com>
2022-03-10 18:58:51 +0530

    some changes done

--- routes/route.js
+++ routes/route.js
@@ -4,6 +4,9 @@ const getimage = require("../component/getimage")
 //endpoint for uploading files
 router.post("/upload",upload.upload);
 router.get('/:uuid',getimage.getImage)
-
+router.get('/', function(req, res){
+    // Rendering home.ejs page
+    res.send('home');
+})
 
 module.exports=router;
