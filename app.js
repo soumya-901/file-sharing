0ffdac20e0615257b59b58879ca3423a2fbe02a2
soumya-901 <sparhi.bsp2018@gmail.com>
2022-06-04 16:14:36 -0400

    may it wo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const DbConnect = require("./database/db");
 const routes = require("./routes/route")
-const PORT = process.env.URL || 3000;
+const PORT = process.env.URL;
 const app = express();
 
 // Data base connection
